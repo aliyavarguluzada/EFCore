7b88204cf3dde350fa2f7e3941cd00a87e46bb80
aliyavar_guluzada <aliyavarguluzada@gmail.com>
2023-07-17 21:12:21 +0400

    Add project files.

---
+++ EFCore/Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using EFCore.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFCore.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source = DESKTOP-9RG21DM\\SQLEXPRESS;Initial Catalog = EFcoreFirstDb;Integrated Security = true;TrustServerCertificate = true");
+        }
+
+
+    }
+}
---
+++ EFCore/Program.cs
@@ -0,0 +1,18 @@
+﻿using EFCore.Models;
+using EFCore.Data;
+
+ApplicationDbContext dbContext = new ApplicationDbContext();
+
+//User user = new User();
+//user.Name = "aliyavar";
+//user.Surname = "guluzada";
+//user.Email = "aliyavarguluzada@gmail.com";
+//user.Password = "aliyavar123";
+//user.Age = 22;
+
+
+User user = dbContext.Users.Where(u => u.Name == "aliyavar").FirstOrDefault();
+user.Name = "mamed";
+//dbContext.Add(user);
+dbContext.SaveChanges();
+
